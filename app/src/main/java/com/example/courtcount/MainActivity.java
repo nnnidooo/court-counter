67493b7d497a2bb201137471f0eaf2109de7ada7
dean <nicklasfrederiksen@gmail.com>
2023-04-28 10:35:08 +0200

    first

--- app/src/main/java/com/example/courtcount/MainActivity.java
+++ app/src/main/java/com/example/courtcount/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.courtcount;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //new ViewModelProvider(this).getClass(ModelClass.class);
     }
 
     /**
